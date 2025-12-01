e2be39ef9665ffb402bb571bc598da0aad5b5453
PhuocSu <phuoclanh78@gmail.com>
2025-12-01 22:15:06 +0700

    feat: add helmet middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // console.log('JWT_SECRET:', process.env.JWT_SECRET);
-  // console.log('Tất cả biến môi trường:', process.env);
+  // Thêm helmet middleware để bảo mật HTTP headers
+  
+app.use(helmet({
+  crossOriginEmbedderPolicy: false,
+  contentSecurityPolicy: {
+    directives: {
+      imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
+      scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
+      frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+    },
+  },
+}));
+
 
   //đăng ký để sử dụng DTO
   app.useGlobalPipes(new ValidationPipe({
