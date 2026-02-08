43da1eb033fe8d313169d3bad0082fd99d1c07ed
PhuocSu <phuoclanh78@gmail.com>
2026-02-08 11:56:49 +0700

    modify: fix the display TopChoice item

--- frontend/src/app/components/ui/homepage/TopChoice.tsx
+++ frontend/src/app/components/ui/homepage/TopChoice.tsx
@@ -12,17 +12,17 @@ const TopChoice = () => {
     if (error) return <div>Error loading top cars</div>;
 
     return (
-    <div style={{ width: "1200px", margin: "0 auto" }}>
+    <div style={{ width: "1200px", margin: "0 auto", boxSizing: "border-box" }}>
       <Flex vertical gap={20}>
         <Typography.Text>Top Choice</Typography.Text>
 
-        <Row gutter={[16, 16]} style={{ width: "100%" }}>
+        <Flex gap={20} style={{ width: "100%" }}>
           {data?.map((car: any, index: number) => (
-            <Col span={8} key={car.id}>
+            <div key={car.id}>
               <CarCard vehicle={car} rankIndex={index}/>
-            </Col>
+            </div>
           ))}
-        </Row>
+        </Flex>
       </Flex>
     </div>
   );
--- frontend/src/app/productDetail/page.tsx
+++ frontend/src/app/productDetail/page.tsx
@@ -1,13 +1,32 @@
-"use client"
+"use client";
 
+import { useRecentlyViewed } from "@/hooks/useRecentlyViewed";
 import ProductDetail from "../components/ui/productDetail/ProductDetail";
+import RecentlyViewed from "../components/ui/sideBar/RecentlyViewed/RecentlyViewed";
+import RightSidebar from "../components/ui/sideBar/RightSidebar";
+import Compare from "../components/ui/sideBar/Compare/Compare";
 
 const ProductDetailPage = () => {
-    return (
-        <div style={{ width: "1200px", height: "100%", margin: "40px auto", display: "flex", gap: "20px" }}>
-            <ProductDetail />
-        </div>
-    );
+  const { isOpen, open, close } = useRecentlyViewed();
+
+  return (
+    <div
+      style={{
+        width: "1200px",
+        height: "100%",
+        margin: "40px auto",
+        display: "flex",
+        gap: "20px",
+      }}
+    >
+      <ProductDetail />
+
+      <RightSidebar onOpen={open} />
+      {isOpen && <RecentlyViewed onClose={close} />}
+      <Compare />
+
+    </div>
+  );
 };
 
 export default ProductDetailPage;
