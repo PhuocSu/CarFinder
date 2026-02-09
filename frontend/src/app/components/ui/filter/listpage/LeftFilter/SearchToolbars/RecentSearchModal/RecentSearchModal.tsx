a57f42c74876b22ae3c5e89dddc8c5e2a73d099f
unknown <phuoclanh78@gmail.com>
2026-02-09 16:48:58 +0700

    feat: add click item => set initial state in LeftFilter ListPage

--- frontend/src/app/components/ui/filter/listpage/LeftFilter/SearchToolbars/RecentSearchModal/RecentSearchModal.tsx
+++ frontend/src/app/components/ui/filter/listpage/LeftFilter/SearchToolbars/RecentSearchModal/RecentSearchModal.tsx
@@ -4,6 +4,7 @@ import { useModelQuery } from "@/app/api/listPage/useModelQuery";
 import { useDeleteRecentSearchMutation } from "@/app/api/recentlySearchHistory/useDeleteRecentSearchMutation";
 import { useRecentSearchHistoryQuery } from "@/app/api/recentlySearchHistory/useRecentSearchHistoryQuery";
 import { VEHICLE_BADGE_OPTIONS } from "@/constants/homepage/vehicleBadgeOptions";
+import { useVehicleFilter } from "@/hooks/useVehicleFilter";
 import { formatSearchFilterTokens } from "@/utils/formatSearchFilter";
 import { CloseOutlined } from "@ant-design/icons";
 import { Flex, Modal } from "antd";
@@ -18,6 +19,31 @@ const RecentSearchModal = ({
   const { data: models = [] } = useModelQuery();
   const { data: history = [] } = useRecentSearchHistoryQuery();
   const { mutate: deleteHistory } = useDeleteRecentSearchMutation();
+  const { updateFilter } = useVehicleFilter();
+
+  const handleItemClick = (item: any, onClose: () => void) => {
+    const filters = item.filters ?? item;
+
+    updateFilter({
+      search: filters.search || "",
+      badges: filters.badges || [],
+      modelIds: filters.modelIds || [],
+      subModelIds: filters.subModelIds || [],
+      yearMin: filters.yearMin,
+      yearMax: filters.yearMax,
+      priceMin: filters.priceMin,
+      priceMax: filters.priceMax,
+      mileageMin: filters.mileageMin,
+      mileageMax: filters.mileageMax,
+      fuelTypes: (filters.fuelTypes ?? filters.fuelTypes) || [],
+      exColors: filters.exColors || [],
+      inColors: filters.inColors || [],
+      sortBy: filters.sortBy,
+      order: filters.order,
+    });
+
+    onClose(); // Close modal
+  };
 
   const renderTokens = (ts: ReturnType<typeof formatSearchFilterTokens>) => (
     <div>
@@ -86,6 +112,7 @@ const RecentSearchModal = ({
                   border:
                     "1px solid var(--base-stroke-color-base-stroke-20, #E0E0E3)",
                 }}
+                onClick={() => handleItemClick(item, onClose)}
               >
                 <Flex vertical>
                   {renderTokens(line1Tokens)}
