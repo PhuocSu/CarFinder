6f0f630a46400cf070b2ab994275c99ca7d6296f
PhuocSu <phuoclanh78@gmail.com>
2026-03-27 22:38:36 +0700

    modify TopChoice attributes

--- frontend/src/app/components/ui/homepage/TopChoice.tsx
+++ frontend/src/app/components/ui/homepage/TopChoice.tsx
@@ -6,24 +6,46 @@ import { Typography } from "antd";
 import CarCard from "../cards/CarCard";
 
 const TopChoice = () => {
-    const {data, isLoading, error} = useTopFavoriteQuery(3);
+  const { data, isLoading, error } = useTopFavoriteQuery(3);
 
-    if (isLoading) return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <img src="/images/loadcat.gif" alt="Loading..." style={{ width: '100px', height: '100px' }} />
-        </div>
-    );
-    if (error) return <div>Error loading top cars</div>;
-
+  if (isLoading)
     return (
-    <div style={{ width: "1200px", margin: "0 auto", boxSizing: "border-box" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <img
+          src="/images/loadcat.gif"
+          alt="Loading..."
+          style={{ width: "100px", height: "100px" }}
+        />
+      </div>
+    );
+  if (error) return <div>Error loading top cars</div>;
+
+  return (
+    <div style={{ width: "1200px", margin: "0 auto", boxSizing: "border-box", marginTop: "60px" }}>
       <Flex vertical gap={20}>
-        <Typography.Text>Top Choice</Typography.Text>
+        <Typography.Text
+          style={{
+            color: "var(--base-fg-color-base-fg-80, #111725)",
+            fontSize: 24,
+            fontFamily: "Noto Sans KR",
+            fontWeight: "700",
+            wordWrap: "break-word",
+          }}
+        >
+          Top Choice
+        </Typography.Text>
 
         <Flex gap={20} style={{ width: "100%" }}>
           {data?.map((car: any, index: number) => (
             <div key={car.id}>
-              <CarCard vehicle={car} rankIndex={index}/>
+              <CarCard vehicle={car} rankIndex={index} />
             </div>
           ))}
         </Flex>
